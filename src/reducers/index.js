001d49f8a27686b3d58041acfb084fc7254261e9
Quoc Khanh <khanhonlyc@gmail.com>
2021-06-06 17:10:30 +0900

    commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import products from './products';
+import cart from './cart';
 //import me from './../constants/Message';
 
-const appReducers = combineReducers({products});
+const appReducers = combineReducers({products, cart});
 
 export default appReducers;
